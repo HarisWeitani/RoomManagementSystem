5d280bcc602d37a0402cfbf484f905c65c1cfe80
sirwarfox <hweitani@gmail.com>
2019-08-08 22:06:57 +0700

    Activity main UI 85%

--- app/src/main/java/com/hw/rms/roommanagementsystem/MainActivity.kt
+++ app/src/main/java/com/hw/rms/roommanagementsystem/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.hw.rms.roommanagementsystem
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.view.Window
@@ -8,18 +9,22 @@ import android.view.WindowManager
 import android.widget.Button
 import android.widget.TextClock
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.denzcoskun.imageslider.ImageSlider
-import com.denzcoskun.imageslider.models.SlideModel
+import com.asura.library.posters.DrawableImage
+import com.asura.library.posters.Poster
+import com.asura.library.posters.RawVideo
+import com.asura.library.views.PosterSlider
 
 class MainActivity : AppCompatActivity() {
 
     private var SAMPLE_LONG_TEXT: String = "The quick brown fox jumps over the lazy dog "
 
     lateinit var tv_running_text : TextView
-    lateinit var tc_enter_admin : TextClock
+    lateinit var tv_enter_admin : TextView
     lateinit var btn_status : Button
+
+    lateinit var posterSlider: PosterSlider
+    var posters : ArrayList<Poster> = arrayListOf()
 
     private fun hideStatusBar(){
         requestWindowFeature(Window.FEATURE_NO_TITLE)
@@ -30,7 +35,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         hideStatusBar()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_available)
         initView()
         initImageSlider()
 
@@ -42,11 +47,13 @@ class MainActivity : AppCompatActivity() {
         //running text below screen
         tv_running_text = findViewById(R.id.tv_running_text)
         tv_running_text.isSelected = true
-        tv_running_text.text = SAMPLE_LONG_TEXT + SAMPLE_LONG_TEXT + SAMPLE_LONG_TEXT + SAMPLE_LONG_TEXT
+        tv_running_text.text = SAMPLE_LONG_TEXT + SAMPLE_LONG_TEXT +
+                SAMPLE_LONG_TEXT + SAMPLE_LONG_TEXT + SAMPLE_LONG_TEXT +
+                SAMPLE_LONG_TEXT + SAMPLE_LONG_TEXT
 
         //clock
-        tc_enter_admin = findViewById(R.id.tc_enter_admin)
-        tc_enter_admin.setOnLongClickListener {
+        tv_enter_admin = findViewById(R.id.tv_enter_admin)
+        tv_enter_admin.setOnLongClickListener {
             val intent = Intent(this@MainActivity,AdminLoginActivity::class.java)
             startActivity(intent)
             true
@@ -60,15 +67,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun initImageSlider(){
-        val imageList = ArrayList<SlideModel>()
-// imageList.add(SlideModel("String Url" or R.drawable)
-// imageList.add(SlideModel("String Url" or R.drawable, "title") You can add title
-// imageList.add(SlideModel("String Url" or R.drawable, "title", true) Also you can add centerCrop scaleType for this image
-        imageList.add(SlideModel(R.drawable.fox, centerCrop = true))
-        imageList.add(SlideModel(R.drawable.elephant,centerCrop = true))
-        imageList.add(SlideModel(R.drawable.tiger, centerCrop = true))
-        val imageSlider = findViewById<ImageSlider>(R.id.image_slider)
-        imageSlider.setImageList(imageList)
+        //can be set by server
+        posterSlider = findViewById(R.id.poster_slider)
+        posters.add(DrawableImage(R.drawable.fox))
+        posters.add(RawVideo(R.raw.seven_sec))
+
+        posterSlider.setPosters(posters)
+
     }
 
     fun buttonListener() {
