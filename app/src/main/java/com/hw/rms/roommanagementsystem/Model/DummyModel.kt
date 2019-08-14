029cc25e0c04164eaf0cfda00e3fb1d2917eb508
sirwarfox <hweitani@gmail.com>
2019-08-14 21:59:52 +0700

    Example With Singleton2

---
+++ app/src/main/java/com/hw/rms/roommanagementsystem/Model/DummyModel.kt
@@ -0,0 +1,8 @@
+package com.hw.rms.roommanagementsystem.Model
+
+class DummyModel( company_name: String, company_logo: String) {
+
+    var company_name : String? = company_name
+    var company_logo : String? = company_logo
+
+}
---
+++ app/src/main/java/com/hw/rms/roommanagementsystem/Model/ImageVideo.kt
@@ -0,0 +1,13 @@
+package com.hw.rms.roommanagementsystem.Model
+
+import android.graphics.Bitmap
+
+class ImageVideo(imageTitle : String,imageUrl: String, videoTitle: String, videoUrl: String) {
+
+    var imageTitle: String? = imageTitle
+    var imageUrl: String? = imageUrl
+
+    var videoTitle: String? = videoTitle
+    var videoUrl: String? = videoUrl
+
+}
